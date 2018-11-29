e213cb1a25e9a8320c84fd7699a31f616b81bc16
zhouqi2 <zhouqi2@wps.cn>
2018-11-29 15:45:38 +0800

    belta
    belta

---
+++ build/precache-manifest.21f4060829893291efcdccd6b43a710b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TragedyGo/websit/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e60fdaad608d41b4a9d8",
+    "url": "/TragedyGo/websit/static/js/runtime~main.e60fdaad.js"
+  },
+  {
+    "revision": "560d545cb905fa8029f7",
+    "url": "/TragedyGo/websit/static/js/main.560d545c.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/TragedyGo/websit/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "560d545cb905fa8029f7",
+    "url": "/TragedyGo/websit/static/css/main.473f8c83.chunk.css"
+  },
+  {
+    "revision": "0fb81d673cc07049a9b02d53a3f09394",
+    "url": "/TragedyGo/websit/index.html"
+  }
+];
